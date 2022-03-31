1d42d8357de41d3ce0eb184fbf173c9cbfdf18d6
Pierre-Anthony Lemieux <pal@palemieux.com>
2022-03-30 17:31:34 -0700

    Added support for ARRI MetaExtract CSV files

---
+++ src/main/python/camdkit/arri/cli.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Copyright (c) 2022, Sandflow Consulting LLC
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+'''ARRI CLI tool'''
+
+import json
+import argparse
+import camdkit.arri.reader
+
+def main():
+  parser = argparse.ArgumentParser(description="Converts ARRI camera metadata to JSON according to the OSVP Camera Metadata Model.")
+  parser.add_argument('csv_path', type=str, help='Path the CSV file extracted using ARRI Meta Extract (AME)')
+
+  args = parser.parse_args()
+
+  model = camdkit.arri.reader.to_clip(args.csv_path)
+
+  print(json.dumps(model.serialize(), indent=2))
+
+if __name__ == "__main__":
+  main()
---
+++ src/main/python/camdkit/arri/reader.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Copyright (c) 2022, Sandflow Consulting LLC
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+'''ARRI camera metadata reader'''
+
+import csv
+from fractions import Fraction
+
+import camdkit.model
+
+def to_clip(csv_path: str) -> camdkit.model.Clip:
+  """Read ARRI camera metadata into a `Clip`. `csv_path` is the path to a CSV
+  file extracted using ARRI Meta Extract (AME)."""
+
+  with open(csv_path, encoding="utf-8") as csvfile:
+    csv_data = list(csv.DictReader(csvfile, dialect="excel-tab"))
+
+    n_frames = len(csv_data)
+
+    if n_frames <= 0:
+      raise ValueError("No data")
+
+    clip = camdkit.model.Clip()
+    clip.set_iso(int(csv_data[0]["Exposure Index ASA"]))
+    clip.set_duration(len(csv_data)/Fraction(csv_data[0]["Project FPS"]))
+    clip.set_focal_length(tuple(float(m["Lens Focal Length"]) for m in csv_data))
+
+  return clip
