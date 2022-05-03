4bbb3bf035a987b9d8d5c918713d46eaea76bcfd
Pierre-Anthony Lemieux <pal@palemieux.com>
2022-05-02 21:51:05 -0700

    arri: added sensor physical dimensions (#6)

--- src/main/python/camdkit/arri/reader.py
+++ src/main/python/camdkit/arri/reader.py
@@ -30,6 +30,15 @@ from fractions import Fraction
 
 import camdkit.model
 
+
+# https://www.arri.com/resource/blob/31908/14147b455c90a9a35018c0d091350ff3/2021-10-arri-formatsandresolutionsoverview-3-4-data.pdf
+_CAMERA_FAMILY_PIXEL_PITCH_MAP = {
+  ("ALEXALF", 1920) : Fraction(316800, 1920),
+  ("ALEXALF", 2048) : Fraction(316800, 2048),
+  ("ALEXALF", 3840) : Fraction(316800, 3840),
+  ("ALEXALF", 4448) : Fraction(367000, 4448),
+}
+
 def to_clip(csv_path: str) -> camdkit.model.Clip:
   """Read ARRI camera metadata into a `Clip`. `csv_path` is the path to a CSV
   file extracted using ARRI Meta Extract (AME)."""
@@ -47,13 +56,26 @@ def to_clip(csv_path: str) -> camdkit.model.Clip:
     assert csv_data[0]["Lens Distance Unit"] == "Meter"
 
     clip.set_iso(int(csv_data[0]["Exposure Index ASA"]))
+
     clip.set_duration(len(csv_data)/Fraction(csv_data[0]["Project FPS"]))
+
     clip.set_lens_serial_number(csv_data[0]["Lens Serial Number"])
+
     clip.set_fps(Fraction(csv_data[0]["Project FPS"]))
+
     clip.set_sensor_pixel_dimensions(
       camdkit.model.SensorPixelDimensions(
         width=int(csv_data[0]["Image Width"]),
         height=int(csv_data[0]["Image Height"])
+      )
+    )
+
+    pix_dims = clip.get_sensor_pixel_dimensions()
+    pixel_pitch = _CAMERA_FAMILY_PIXEL_PITCH_MAP[(csv_data[0]["Camera Family"], pix_dims.width)]
+    clip.set_sensor_physical_dimensions(
+      camdkit.model.SensorPhysicalDimensions(
+        width=round(pix_dims.width * pixel_pitch),
+        height=round(pix_dims.height * pixel_pitch)
       )
     )
 
